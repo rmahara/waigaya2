28f687d962f674365f57a02e4aeb8606d643c2d6
PSCAD\09688 <snakamura@pacific-systems.co.jp>
2024-04-12 17:01:48 +0900

    バックエンド実装

---
+++ 2_snakamura/2_Step5_コントローラーの作成(ハンズオン)/Migrations/20240410114750_Initial.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Waigaya2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    price = table.Column<decimal>(type: "decimal(8,2)", nullable: false),
+                    category_id = table.Column<int>(type: "int", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    delete_at = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_category_id",
+                        column: x => x.category_id,
+                        principalTable: "Categories",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "id", "created_at", "title" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 4, 10, 20, 47, 50, 60, DateTimeKind.Local).AddTicks(1510), "回復アイテム" },
+                    { 2, new DateTime(2024, 4, 10, 20, 47, 50, 60, DateTimeKind.Local).AddTicks(1511), "武器" },
+                    { 3, new DateTime(2024, 4, 10, 20, 47, 50, 60, DateTimeKind.Local).AddTicks(1512), "防具" },
+                    { 4, new DateTime(2024, 4, 10, 20, 47, 50, 60, DateTimeKind.Local).AddTicks(1513), "家電" },
+                    { 5, new DateTime(2024, 4, 10, 20, 47, 50, 60, DateTimeKind.Local).AddTicks(1515), "生活用品" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "id", "category_id", "created_at", "delete_at", "description", "name", "price" },
+                values: new object[,]
+                {
+                    { 1, 2, new DateTime(2024, 4, 10, 20, 47, 50, 60, DateTimeKind.Local).AddTicks(1610), null, "エクスカリバーには、普通の剣には無い特別な力が備わって\r\nいたと考えられています。敵の攻撃を跳ね返す魔力や、所持\r\n者に無敵の力を与える等の能力が伝承されています。", "エクスカリバー", 54000m },
+                    { 2, 3, new DateTime(2024, 4, 10, 20, 47, 50, 60, DateTimeKind.Local).AddTicks(1612), null, "ユニクロで売ってます", "ヒートテック", 2000m },
+                    { 3, 1, new DateTime(2024, 4, 10, 20, 47, 50, 60, DateTimeKind.Local).AddTicks(1614), null, "ダイソンではない", "掃除機", 30000m },
+                    { 4, 5, new DateTime(2024, 4, 10, 20, 47, 50, 60, DateTimeKind.Local).AddTicks(1615), null, "たんぱく質を英語でいうとプロテイン", "プロテイン", 4000m },
+                    { 5, 5, new DateTime(2024, 4, 10, 20, 47, 50, 60, DateTimeKind.Local).AddTicks(1617), null, "ただの水です", "500mlの水", 100m }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_category_id",
+                table: "Products",
+                column: "category_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
---
+++ 2_snakamura/2_StepLastTest/Controllers/Request/SaveProduct.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Waigaya2.Models;
+
+namespace Waigaya2.Controllers.Request
+{
+    //JSONを受取るためのメソッド
+    public class SaveProduct
+    {
+        public int? Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public string Description { get; set; } = "";
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
