cdcba3690c66cce1bec8c2b7940a677ae451c8de
mahara <rmahara@pacific-systems.co.jp>
2024-04-15 15:47:18 +0900

    コントローラーを追加

---
+++ 2_snakamura/2_Step5_コントローラーの作成(ハンズオン)/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Waigaya2.Data;
+
+namespace Waigaya2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly WaigayaDbContext _context;
+
+        public ProductsController(WaigayaDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// 商品マスタ全件表示
+        /// </summary>
+        /// <returns></returns>
+        [Route("")]
+        [Route("productlist")]
+        public IActionResult ProductList()
+        {
+            var list = _context.Products.Include(p => p.Category).Where(p => p.DeleteAt == null).ToList();
+            return View(list);
+        }
+    }
+}
